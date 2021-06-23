a207af67a75ce1725b0f2f2ea3d146c526d12827
LILERNEST <ejchisholm06@gmail.com>
2021-06-23 16:13:33 +0000

    adding all work

---
+++ projects/platformer/js/factory/platform.js
@@ -0,0 +1,22 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    let opspark = window.opspark;
+    window.opspark.platform = window.opspark.platform || {};
+    
+    opspark.platform.factory = function (game) {
+        game.platforms = game.add.group();
+        game.platforms.enableBody = true;
+        
+        function create(x, y, scaleX, scaleY, immovable) {
+            var platform = game.platforms.create(x, y, 'platform');
+            platform.scale.setTo(scaleX || 1, scaleY || 1);
+            platform.body.immovable = immovable || true;
+            return platform;
+        }
+        opspark.platform.create = create; 
+        createPlatform(400, 460);   
+createPlatform(300, 200, 0.3)       
+           
+    };
+})(window);
